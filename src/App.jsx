7133889ea61c3718994556caefe77e39a99cbf3f
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-03-20 15:28:59 +0100

    feat: hero and header added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomeScreen />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
